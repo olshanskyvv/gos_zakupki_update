0ecd0225ebe21745b381509464d2a22eb65fb78b
olshanskyvv <90987935+olshanskyvv@users.noreply.github.com>
2024-12-07 00:15:08 +0300

    Initial commit

---
+++ src/main/java/ru/miit/goszakupki/GosZakupkiApplication.java
@@ -0,0 +1,13 @@
+package ru.miit.goszakupki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GosZakupkiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GosZakupkiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/miit/goszakupki/GosZakupkiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.miit.goszakupki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GosZakupkiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
